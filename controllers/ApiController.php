9643ffe755b3225ca64fbb30614e77ae800a3569
daxionginfo@gmail.com <daxionginfo@gmail.com>
2018-08-10 08:14:59 +0700

    insert properties

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -7,6 +7,13 @@ use yii\web\Response;
 use yii\httpclient\Client;
 use app\models\Properties;
 use app\models\Photo;
+use app\models\PropertiesType;
+use yii\web\UploadedFile;
+use app\models\Packages;
+use app\models\PropertiesDetail;
+use yii\imagine\Image;
+use Imagine\Image\Box;
+
 class ApiController extends \yii\web\Controller
 {
     public function actionIndex()
@@ -155,7 +162,7 @@ class ApiController extends \yii\web\Controller
     public function actionDetailhouse($id)
     {
         $model=Properties::find()
-        ->select('properties.id ,
+        ->select('properties.* ,
         pd.details,
         pd.id as did,
         pd.status as dstatus,
@@ -175,9 +182,126 @@ class ApiController extends \yii\web\Controller
     }
     public function actionPhotos($did)
     {
-        $photos=Photo::find()->where(['properties_detail_id'=>$did])->asArray()->all();
+        $photos=Photo::find()->select('photo.name as name')->where(['properties_detail_id'=>$did])->asArray()->all();
        \Yii::$app->response->format = Response::FORMAT_JSON;
         $row=['photos'=>$photos];
         return $row;
     }
+
+    public function actionListhouseuser($id)
+    {
+        $model=Properties::find()
+        ->select('properties.id ,
+        pd.details,
+        pd.id as did,
+        pd.status as dstatus,
+        pd.fee,
+        pd.per,
+        type.name as type_name,
+        photo.name as photo_name
+        ')
+        ->joinWith(['propertiesDetails as pd',
+        'propertiesType as type',
+        'propertiesDetails.photos as photo'])
+        ->where(['user_id'=>$id])
+        ->asArray()->all();
+        
+       \Yii::$app->response->format = Response::FORMAT_JSON;
+        $row=['rows'=>$model];
+        return $row;
+    }
+
+    public function actionListpropertiestype(){
+        $model=PropertiesType::find()->all();
+        \Yii::$app->response->format = Response::FORMAT_JSON;
+        $row=['rows'=>$model];
+        return $row;
+    }
+
+    public function actionListpackage(){
+        $model=Packages::find()->all();
+        \Yii::$app->response->format = Response::FORMAT_JSON;
+        $row=['rows'=>$model];
+        return $row;
+    }
+
+    public function actionUplaodfile(){
+
+        $uploads = UploadedFile::getInstancesByName("upfile");
+            if (empty($uploads)){
+                return "Must upload at least 1 file in upfile form-data POST";
+            }
+
+            // $uploads now contains 1 or more UploadedFile instances
+            $savedfiles =null;
+            foreach ($uploads as $file){
+                $realFileName = rand().date('Ymdhis').time().'.'.$file->extension;
+                $path =\Yii::$app->basePath.'/web/images/'.$realFileName; //Generate your save file path here;
+                if($file->saveAs($path)){
+                    $savedfiles=$realFileName;
+                    $imagine = Image::getImagine();
+                    $image = $imagine->open(\Yii::$app->basePath.'/web/images/'.$savedfiles);
+                    $image->resize(new Box(500, 300))->save(\Yii::$app->basePath.'/web/images/small/'.$savedfiles, ['quality' => 70]);
+                exit;}else{
+                    $savedfiles='Error save file';
+                } //Your uploaded file is saved, you can process it further from here
+            }
+            \Yii::$app->response->format = Response::FORMAT_JSON;
+            return $savedfiles;
+
+    }
+
+    public function actionCreateproperties()
+    {
+    
+        $model = new Properties();
+        $propertiesdetail = new PropertiesDetail();
+        if (isset($_POST)) {
+            $model->packages_id =1;
+            $model->date_create = date('Y-m-d H:i:s');
+            $model->user_id =$_POST['userID'];
+            $propertiesdetail->details=$_POST['details'];
+            $propertiesdetail->fee=$_POST['fee'];
+            $model->date_start=date('Y-m-d');
+            $model->user_id=$_POST['userID'];
+            if($_POST['per']=='ເດືອນ')
+            {
+                $propertiesdetail->per='m';
+            }else{
+                $propertiesdetail->per='y';
+            }
+            $type = PropertiesType::find()->where(['name' =>$_POST['propertye']])->one();
+            if(!empty($type))
+            {
+                $model->properties_type_id=$type->id;
+            }
+
+            $package = Packages::find()->where(['id' =>1])->one();
+            if ($package->m_y == 'm') {
+                $enddate = date('Y-m-d', strtotime('+'.$package->number.' month', strtotime($model->date_start)));
+            } else {
+                $enddate = date('Y-m-d', strtotime('+'.$package->number.' year', strtotime($model->date_start)));
+            }
+            $model->date_end = $enddate;
+            $model->status = 1;
+            if ($model->save()) {
+                $propertiesdetail->properties_id = $model->id;
+                if ($propertiesdetail->save() && !empty($_POST['photos'])) {
+                   
+                   $re=str_replace("[","",$_POST['photos']);
+                   $re=str_replace("]","",$re);
+                    foreach (explode(",",$re) as $photo) {
+                        $dphoto = new Photo();
+                        $dphoto->properties_detail_id = $propertiesdetail->id;
+                        $dphoto->name = preg_replace('/\s+/', '', $photo);
+                        $dphoto->save();
+                    }
+                    \Yii::$app->response->format = Response::FORMAT_JSON;
+                    return ['id'=>$model->id,'did'=>$propertiesdetail->id];
+                }
+            }
+        }
+        
+    }
+
 }
